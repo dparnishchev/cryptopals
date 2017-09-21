587244c6db2560cb38bf9714e3408213d25cc92e
Krouler <overloadedd@gmail.com>
2017-09-21 16:25:30 +0300

    Added pkcs7.unpad() function Padding/unpadding in AES encrypt/decrypt procedures Sdded solution to ch11

--- aes.py
+++ aes.py
@@ -1,24 +1,39 @@
 import os
 import base64
 import xor
+import pkcs7
 from Crypto.Cipher import AES
 
 def decrypt_ecb_128(ciphertext, key):
-	return AES.new(key, AES.MODE_ECB, "").decrypt(ciphertext)
+	return pkcs7.unpad(AES.new(key, AES.MODE_ECB, "").decrypt(ciphertext))
 
 def encrypt_ecb_128(plaintext, key):
-	return AES.new(key, AES.MODE_ECB, "").encrypt(plaintext)
+	return AES.new(key, AES.MODE_ECB, "").encrypt(pkcs7.pad(plaintext, 16))
 
 def decrypt_cbc_128(ciphertext, iv, key):
+	cipher = AES.new(key, AES.MODE_ECB, "")
 	plaintext = ""
 	for i in range(0, len(ciphertext) / 16):
 		block = ciphertext[16 * i : 16 * (i + 1)]
 		if i == 0:
-			plaintext = plaintext + xor.xor(decrypt_ecb_128(block, key), iv)
+			plaintext = plaintext + xor.xor(cipher.decrypt(block), iv)
 		else:
 			prev_block = ciphertext[16 * (i - 1): 16 * i]
-			plaintext = plaintext + xor.xor(decrypt_ecb_128(block, key), prev_block)
-	return plaintext
+			plaintext = plaintext + xor.xor(cipher.decrypt(block), prev_block)
+	return pkcs7.unpad(plaintext)
+
+def encrypt_cbc_128(plaintext, iv, key):
+	cipher = AES.new(key, AES.MODE_ECB, "")
+	ciphertext = ""
+	plaintext = pkcs7.pad(plaintext, 16)
+	for i in range(0, len(plaintext) / 16):
+		block = plaintext[16 * i : 16 * (i + 1)]
+		if i == 0:
+			ciphertext = ciphertext + cipher.encrypt(xor.xor(block, iv))
+		else:
+			prev_block = ciphertext[16 * (i - 1): 16 * i]
+			ciphertext = ciphertext + cipher.encrypt(xor.xor(block, prev_block))
+	return ciphertext
 
 #detects the score of a ciphertext. Higher score means 
 #more chance that this text was encrypted by AES-128 in ECB mode
@@ -31,12 +46,17 @@ def score_ecb_128(ciphertext):
 		ciphertext = ciphertext[16:]
 	return score
 
-def random_key():
-	return os.urandom(16)
-
 def encryption_oracle(plaintext):
-	key = random_key()
-
+	key = os.urandom(16)
+	alg = ord(os.urandom(1)) % 2
+	prefix = os.urandom(5 + (ord(os.urandom(1)) % 6))
+	postfix = os.urandom(5 + (ord(os.urandom(1)) % 6))
+	plaintext = prefix + plaintext + postfix
+	if(alg == 0):
+		return (encrypt_ecb_128(plaintext, key), 0)
+	else:
+		iv = os.urandom(16)
+		return (encrypt_cbc_128(plaintext, iv, key), 1)
 
 def _solve_set1_ch7():
 	file = open("./input/7.txt", "r")
@@ -62,5 +82,17 @@ def _solve_set2_ch10():
 	file = open("./input/10.txt", "r")
 	data = file.read().replace("\n", "")
 	file.close()
-	print decrypt_cbc_128(base64.decode(data), "00" * 16, "YELLOW SUBMARINE")
+	return decrypt_cbc_128(base64.decode(data), "\x00" * 16, "YELLOW SUBMARINE")
 
+def _solve_set2_ch11():
+	plaintext = _solve_set2_ch10()
+	for i in range(0, 10):
+		(ciphertext, alg) = encryption_oracle(plaintext)
+		if alg == 0:
+			alg = "ECB"
+		else:
+			alg = "CBC"
+		if score_ecb_128(ciphertext) > 0:
+			print str(i) + " guessed alg is ECB (actual is {:s})".format(alg)
+		else:
+			print str(i) + " guessed alg is CBC (actual is {:s})".format(alg)
--- pkcs7.py
+++ pkcs7.py
@@ -4,5 +4,16 @@ def pad(data, block_size):
 	pad_size = block_size - (len(data) % block_size)
 	return data + chr(pad_size) * pad_size
 
+def unpad(data):
+	pad_size = ord(data[-1])
+	for i in range(-2, -pad_size - 1, -1):
+		if(data[i] != data[-1]):
+			return data
+	return data[:len(data) - pad_size]
+
+
 def _solve_set2_ch9():
-	return pad("YELLOW SUBMARINE", 20)
+	padded =  pad("YELLOW SUBMARINE", 20)
+	unpadded = unpad(padded)
+	print unpadded
+	print padded
